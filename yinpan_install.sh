cefc2eb74251865e4fdd4463f467c2948216b6cf
zhaocai-ally <54318393+zhaocai-ally@users.noreply.github.com>
2020-10-29 21:38:08 +0800

    Update yinpan_install.sh

--- yinpan_install.sh
+++ yinpan_install.sh
@@ -67,3 +67,10 @@ pacman -S fcitx-configtool
 pacman -S fcitx-gtk2 fcitx-gtk3 fcitx-qt5 kcm-fcitx fcitx-im
 pacman -S fcitx-googlepinyin
 
+echo -e "${w}    _             _       _     _                    ${h}"
+echo -e "${g}   / \   _ __ ___| |__   | |   (_)_ __  _   ___  _   ${h}"
+echo -e "${b}  / _ \ | '__/ __| '_ \  | |   | | '_ \| | | \ \/ /  ${h}"
+echo -e "${y} / ___ \| | | (__| | | | | |___| | | | | |_| |>  <   ${h}"
+echo -e "${r}/_/   \_\_|  \___|_| |_| |_____|_|_| |_|\__,_/_/\_\  ${h}"
+echo ".....................................OK"
+echo ".....................................OK"
