c249aec8f47ec70432f7d12c3961b7aae92e8bcb
zhaocai-ally <54318393+zhaocai-ally@users.noreply.github.com>
2020-10-29 19:32:10 +0800

    Update root_install.sh

--- root_install.sh
+++ root_install.sh
@@ -9,9 +9,11 @@ echo -e "${r}/_/   \_\_|  \___|_| |_| |_____|_|_| |_|\__,_/_/\_\  ${h}"
 rm -rf /etc/locale.gen
 cp ./locale.gen /etc/
 locale-gen
+echo "切换为国内源"
 rm -rf /etc/pacman.d/mirrorlist
 cp ./mirrorlist /etc/pacman.d/
 echo ".....................................OK"
+echo "安装系统组件按回车键（Enter）确认"
 pacstrap /mnt base linux linux-firmware base-devel vi vim networkmanager git
 echo ".....................................OK"
 genfstab -U -p /mnt >> /mnt/etc/fstab
